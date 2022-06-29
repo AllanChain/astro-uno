e4a5214d0c9fb718cf5deea1171e36395285b510
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-06-29 16:45:23 +0800

    feat: add attributify mode fix

--- astro-uno/src/index.ts
+++ astro-uno/src/index.ts
@@ -8,13 +8,26 @@ export = function UnoIntegration (options: VitePluginConfig): AstroIntegration {
     name: 'astro-uno',
     hooks: {
       'astro:config:setup': ({ updateConfig }) => {
+        const attributifyPreset = options.presets.find(preset =>
+          !Array.isArray(preset) && preset.name === '@unocss/preset-attributify'
+        )
+        if (!Array.isArray(attributifyPreset) && attributifyPreset) {
+          const attributifyExtractor = attributifyPreset.extractors
+            .find(extractor => extractor.name === 'attributify')
+          const extract = attributifyExtractor.extract
+          attributifyExtractor.extract = ({ code, original }) => extract({
+            // Adding a space here, otherwise UnoCSS doesn't recognize the tag
+            code: code.replace(/(<\w+)(\$\{\$\$addAttribute)/, '$1 $2'),
+            original
+          })
+        }
+
         const unocssPlugins = Unocss(options)
         unocssPlugins.find(
           plugin => plugin.name === 'unocss:global:build:generate'
         ).apply = (options, { command }) => {
           return command === 'build' && !!options.build?.ssr
         }
-
         updateConfig({
           vite: {
             resolve: {
--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config'
 
 import vue from '@astrojs/vue'
 import uno from 'astro-uno'
-import { presetUno } from 'unocss'
+import { presetUno, presetAttributify } from 'unocss'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     vue(),
     uno({
-      presets: [presetUno()]
+      presets: [presetUno(), presetAttributify()]
     })
   ]
 })
