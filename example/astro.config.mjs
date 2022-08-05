a321da00a97353b9cbac6bf5d7cc94d56e4ddea2
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-08-06 07:35:33 +0800

    chore(deps): update

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config'
 
-import vue from '@astrojs/vue'
 import uno from 'astro-uno'
 import { presetUno, presetAttributify } from 'unocss'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    vue(),
     uno({
       presets: [presetUno(), presetAttributify()]
     })
